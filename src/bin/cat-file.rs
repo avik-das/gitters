2a4aef7616eb3bd882212eb15dcaaf82ed2dfa46
Avik Das <avik.das@berkeley.edu>
2016-10-31 00:06:19 -0700

    Start parsing arguments to cat-file
    This introduces docopt, which I'll use for all the tools. Now, all the
    necessary flags and arguments are being parsed correctly for "cat-file".

--- src/bin/cat-file.rs
+++ src/bin/cat-file.rs
@@ -1,8 +1,49 @@
 extern crate gitters;
 
-// TODO: start using the docopt library
-use std::env;
+extern crate rustc_serialize;
+extern crate docopt;
+
+use docopt::Docopt;
+
+const USAGE: &'static str = "
+cat-file
+
+Usage:
+  cat-file -t <object>
+  cat-file -s <object>
+  cat-file -e <object>
+  cat-file -p <object>
+  cat-file (-h | --help)
+
+Options:
+  -h --help  Show this screen.
+  -t         Instead of the content, show the object type identified by <object>.
+  -s         Instead of the content, show the object size identified by <object>.
+  -e         Surpress all output; instead exit with zero status if <object> exists and is a valid
+             object.
+  -p         Pretty-print the contents of <object> based on its type.
+";
+
+#[derive(RustcDecodable)]
+struct Args {
+    flag_t: bool,
+    flag_s: bool,
+    flag_e: bool,
+    flag_p: bool,
+    arg_object: String
+}
+
+fn bool_to_yn(b: bool) -> &'static str {
+    if b { "Y" } else { "N" }
+}
 
 fn main() {
-    gitters::cat_file(&env::args().last().unwrap());
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.decode())
+        .unwrap_or_else(|e| e.exit());
+
+    println!("flags: t = {}, s = {}, e = {}, p = {}",
+             bool_to_yn(args.flag_t), bool_to_yn(args.flag_s),
+             bool_to_yn(args.flag_e), bool_to_yn(args.flag_p));
+    gitters::cat_file(&args.arg_object);
 }
