441b9322844abdba9095dfcfd16f564fcf6b9b49
Avik Das <avik.das@berkeley.edu>
2016-12-11 22:43:38 -0800

    Support partial SHA1s as revision specification

--- src/revisions.rs
+++ src/revisions.rs
@@ -4,6 +4,7 @@
 
 use std::error;
 use std::fmt;
+use std::fs;
 use regex::Regex;
 use objects;
 
@@ -42,12 +43,37 @@ impl error::Error for Error {
 pub fn resolve(rev: &str) -> Result<objects::Name, Error> {
     lazy_static! {
         static ref FULL_SHA1_REGEX: Regex = Regex::new(r"^[0-9a-f]{40}$").unwrap();
+        static ref PARTIAL_SHA1_REGEX: Regex = Regex::new(r"^[0-9a-f]{4,39}$").unwrap();
     }
 
-    // Currently, only support full SHA-1s, so all we have to do is check that we have the correct
-    // format, and then return it.
     if FULL_SHA1_REGEX.is_match(rev) {
         return Ok(objects::Name(rev.to_string()));
+    } else if PARTIAL_SHA1_REGEX.is_match(rev) {
+        let prefix = &rev[..2];
+        let suffix = &rev[2..];
+
+        let dir = format!(".git/objects/{}", prefix);
+        let files = try!(fs::read_dir(dir).map_err(|_| Error::InvalidRevision));
+
+        let mut matching_files = Vec::new();
+        for file in files {
+            let filename = try!(file.map_err(|_| Error::InvalidRevision)).file_name();
+            let filename = try!(filename.into_string().map_err(|_| Error::InvalidRevision));
+            if filename.starts_with(suffix) {
+                matching_files.push(filename);
+            }
+        }
+
+        if matching_files.is_empty() {
+            return Err(Error::InvalidRevision);
+        }
+
+        // Because we don't have an example of an ambiguous four-character SHA1, we'll ignore that
+        // case until we find such a partial SHA1.
+        assert!(matching_files.len() == 1);
+
+        let full_sha1 = format!("{}{}", prefix, matching_files[0]);
+        return Ok(objects::Name(full_sha1));
     }
 
     Err(Error::InvalidRevision)
--- tests/revisions_test.rs
+++ tests/revisions_test.rs
@@ -11,6 +11,13 @@ fn full_sha1_resolves_to_self() {
 }
 
 #[test]
+fn partial_sha1_resolves_to_full_sha1_if_unambiguous() {
+    assert_eq!(
+        Ok(objects::Name("4ddb0025ef5914b51fb835495f5259a6d962df21".to_string())),
+        revisions::resolve("4ddb0025e"));
+}
+
+#[test]
 fn invalid_revision_does_not_resolve() {
     assert_eq!(
         Err(revisions::Error::InvalidRevision),
